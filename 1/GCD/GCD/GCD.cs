895d4e7055a4f7440628eef4a766f82bb65f76ed
Gumar Diana <lantan.mp4@gmail.com>
2019-11-10 18:12:45 +0300

    добавлены перегруженные методы для нахождения нод 3, 4 и 5 чисел и тест к ним

--- 1/GCD/GCD/GCD.cs
+++ 1/GCD/GCD/GCD.cs
@@ -30,6 +30,46 @@ namespace GCD
             return 0;
         }
 
+        /// <summary>
+        /// Алгоритм Евклида по вычислению НОД трёх целых чисел 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <returns></returns>
+        public static int getGCD_Euclidean(int x, int y, int z)
+        {
+            //работает по принципу парного вычисления нод
+            return getGCD_Euclidean(getGCD_Euclidean(x, y), z);
+        }
+
+        /// <summary>
+        /// Алгоритм Евклида по вычислению НОД 4-х целых чисел 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <returns></returns>
+        public static int getGCD_Euclidean(int x, int y, int z, int k)
+        {
+            //работает по принципу парного вычисления нод
+            return getGCD_Euclidean(getGCD_Euclidean(getGCD_Euclidean(x, y), z), k);
+        }
+
+        /// <summary>
+        /// Алгоритм Евклида по вычислению НОД пяти целых чисел 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <returns></returns>
+        public static int getGCD_Euclidean(int x, int y, int z, int k, int j)
+        {
+            //работает по принципу парного вычисления нод
+            return getGCD_Euclidean(getGCD_Euclidean(getGCD_Euclidean(getGCD_Euclidean(x, y), z), k), j);
+        }
+
+
         public void getGCDbinaryEuclidean()
         {
 
---
+++ 1/GCD/GCDTests/Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GCDTests
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestgetGCD_Euclidean_2()
+        {          
+
+            //arrange
+            int actual, expected = 4464;
+            int x = 624960, y = 49104;
+
+            //act
+            actual = GCD.GCD.getGCD_Euclidean(x, y);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void TestgetGCD_Euclidean_4()
+        {          
+
+            //arrange
+            int actual, expected = 6;
+            int x = 78, y = 294, z = 570, k = 36;
+
+            //act
+            actual = GCD.GCD.getGCD_Euclidean(x, y, z, k);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
