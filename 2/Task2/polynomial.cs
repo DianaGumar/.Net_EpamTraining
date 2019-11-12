aa661ba7673d6b98b7f5878881a521d2072aa1cb
Gumar Diana <lantan.mp4@gmail.com>
2019-11-12 09:14:59 +0300

    add 2 task project

---
+++ 2/Task2/polynomial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    /// <summary>
+    /// многочлен от одной переменной
+    /// </summary>
+    class polynomial
+    {
+
+
+        double x;
+
+    }
+}
---
+++ 2/Task2/vector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    /// <summary>
+    /// трёхмерный вектор
+    /// </summary>
+    public class vector
+    {
+        vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            
+        }
+
+        double x, y, z;
+
+        public static vector operator +(vector v, vector u) { }
+
+
+    }
+}
