2d8f9751e93dac1e065e59efb9cbeec08bb5711c
Deana Gumar <lantan.mp4@gmail.com>
2019-12-30 03:21:09 +0300

    sm commit

---
+++ 5/BynaryTree/BynaryTree/BynaryTree.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BynaryTree
+{
+    /// <summary>
+    /// jeneric class bynary tree
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class BynaryTree <T> where T : IComparable
+    {
+
+
+        List<T> Data;
+
+        private void Ballans()
+        {
+
+        }
+
+        /// <summary>
+        /// bouble sorted with compareTo method
+        /// </summary>
+        /// <param name="mas"></param>
+        /// <returns></returns>
+        private List<T> Sort(List<T> mas)
+        {
+
+            T temp;
+            for (int i = 0; i < mas.Count - 1; i++)
+            {
+                for (int j = 0; j < mas.Count - i - 1; j++)
+                {
+                    if (mas[j + 1].CompareTo(mas[j]) > 0)
+                    {
+                        temp = mas[j + 1];
+                        mas[j + 1] = mas[j];
+                        mas[j] = temp;
+                    }
+                }
+            }
+            return mas;
+
+        }
+
+        /// <summary>
+        /// add new item
+        /// </summary>
+        /// <param name="t"></param>
+        public void Add(T t)
+        {
+            Data.Add(t);
+        }
+
+        /// <summary>
+        /// print to string sorted
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            Sort(this.Data);
+
+            return base.ToString();
+        }
+
+        /// <summary>
+        /// setialisation to xml
+        /// </summary>
+        /// <returns></returns>
+        public bool ToXML()
+        {
+            Sort(this.Data);
+
+            return true;
+        }
+
+    }
+}
---
+++ 5/BynaryTree/BynaryTree/Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BynaryTree
+{
+    class Test : IComparable
+    {
+        Test(string StudentName, int TestName, DateTime PassTestDate, int Mark)
+        {
+            this.StudentName = StudentName;
+            this.TestName = TestName;
+            this.PassTestDate = PassTestDate;
+            this.Mark = Mark;
+        }
+
+        public string StudentName;
+        public int TestName;
+        public DateTime PassTestDate;
+        public int Mark;
+
+        /// <summary>
+        /// from Icomparable
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public int CompareTo(object obj)
+        {
+            Test t = obj as Test;
+
+            if(t != null)
+            {
+                return this.TestName - t.TestName;
+            }
+            else
+                throw new Exception("Error. Can't compare two objects");
+        }
+    }
+}
